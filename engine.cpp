14b5e9671b70b1deefcb3031c2493a5e6879199e
Haichuan Zhang <me@std4453.com>
2020-06-08 22:46:13 +0800

    Cross lines to center of the screen

--- engine.cpp
+++ engine.cpp
@@ -4,23 +4,52 @@ camera *cam;
 control *ctrl;
 scene_graph sg;
 
+int w_width, w_height;
+
 void reshape(int width, int height) {
     if (height == 0) height = 1;
     glViewport(0, 0, width, height);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    cam -> transform(width, height);
+    w_width = width;
+    w_height = height;
 }
 
 void redraw() {
     update();
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glDisable(GL_BLEND);
+    glEnable(GL_DEPTH_TEST);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    cam -> transform(w_width, w_height);
+
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
     ctrl -> update();
     ray r = ctrl -> get_ray();
     sg.render(r);
+
+    // cross
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0, w_width, 0, w_height, -1, 1);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    glEnable(GL_BLEND);
+    glDisable(GL_DEPTH_TEST);
+    
+    glBlendFunc(GL_ONE_MINUS_DST_COLOR, GL_ONE_MINUS_SRC_COLOR);
+    glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+    glBegin(GL_LINES);
+    glVertex2f(w_width / 2 - 10.0f, w_height / 2);
+    glVertex2f(w_width / 2 + 10.0f, w_height / 2);
+    glVertex2f(w_width / 2, w_height / 2 - 10.0f);
+    glVertex2f(w_width / 2, w_height / 2 + 10.0f);
+    glEnd();
+    glDisable(GL_BLEND);
 
     glutSwapBuffers();
 }
