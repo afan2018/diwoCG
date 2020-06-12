b3bc90e99b2cd6bdf4155b558aa33d5e6e80d27d
FanAndong <anthonyfan2018@163.com>
2020-06-12 15:20:11 +0800

    Try to optimize usage of glu functions

--- geometries.h
+++ geometries.h
@@ -9,38 +9,46 @@
 
 class box : public node {
 private:
-    void drawBox(GLfloat size);
+	void drawBox(GLfloat size);
 
 public:
-    void render() override {
-        transform();
-        colorize();
-        drawBox(1.0f);
-    }
+	void render() override {
+		transform();
+		colorize();
+		drawBox(1.0f);
+	}
 };
 
 class sphere : public node {
 private:
 	GLUquadricObj *objSphere;
+	GLint listId;
 public:
 	sphere() {
 		objSphere = gluNewQuadric();
+
+		gluQuadricDrawStyle(objSphere, GLU_FILL);
+		gluQuadricTexture(objSphere, GL_TRUE);
+		gluQuadricNormals(objSphere, GLU_SMOOTH);
+
+		listId = glGenLists(1);
+		glNewList(listId, GL_COMPILE);
+		gluSphere(objSphere, 0.5, 40, 50);
+		glEndList();
 	}
 
 	~sphere() {
 		gluDeleteQuadric(objSphere);
 	}
 
-    void render() override {
-        transform();
-        colorize();
-		
-		gluQuadricDrawStyle(objSphere, GLU_FILL);
-		gluQuadricTexture(objSphere, GL_TRUE);
-		gluQuadricNormals(objSphere, GLU_SMOOTH);
+	void render() override {
+		transform();
+		colorize();
 
-		gluSphere(objSphere, 0.5, 40, 50);
-    }
+		glPushMatrix();
+		glCallList(listId);
+		glPopMatrix();
+	}
 };
 
 class cylinder : public node {
@@ -52,34 +60,34 @@ private:
 	GLuint listId;
 
 public:
-    cylinder() {
-        center[2] = 0.5f;
+	cylinder() {
+		center[2] = 0.5f;
 		objCylinder = gluNewQuadric();
 		objDiskBottom = gluNewQuadric();
 		objDiskTop = gluNewQuadric();
 
-        const GLfloat radius = 0.5f;
+		const GLfloat radius = 0.5f;
 
-        gluQuadricDrawStyle(objCylinder, GLU_FILL);
-        gluQuadricTexture(objCylinder, GL_TRUE);
-        gluQuadricNormals(objCylinder, GLU_SMOOTH);
-        gluQuadricDrawStyle(objDiskBottom, GLU_FILL);
-        gluQuadricTexture(objDiskBottom, GL_TRUE);
-        gluQuadricNormals(objDiskBottom, GLU_SMOOTH);
-        gluQuadricDrawStyle(objDiskTop, GLU_FILL);
-        gluQuadricTexture(objDiskTop, GL_TRUE);
-        gluQuadricNormals(objDiskTop, GLU_SMOOTH);
+		gluQuadricDrawStyle(objCylinder, GLU_FILL);
+		gluQuadricTexture(objCylinder, GL_TRUE);
+		gluQuadricNormals(objCylinder, GLU_SMOOTH);
+		gluQuadricDrawStyle(objDiskBottom, GLU_FILL);
+		gluQuadricTexture(objDiskBottom, GL_TRUE);
+		gluQuadricNormals(objDiskBottom, GLU_SMOOTH);
+		gluQuadricDrawStyle(objDiskTop, GLU_FILL);
+		gluQuadricTexture(objDiskTop, GL_TRUE);
+		gluQuadricNormals(objDiskTop, GLU_SMOOTH);
 
-        listId =  glGenLists(1);
-        glNewList(listId, GL_COMPILE);
-        gluCylinder(objCylinder, radius, radius, 1.0, 40, 5);
-        gluDisk(objDiskBottom, 0, radius, 40, 50);
+		listId = glGenLists(1);
+		glNewList(listId, GL_COMPILE);
+		gluCylinder(objCylinder, radius, radius, 1.0, 40, 5);
+		gluDisk(objDiskBottom, 0, radius, 40, 50);
 
-        glTranslatef(0, 0, 1.0);
-        gluDisk(objDiskTop, 0, radius, 40, 50);
+		glTranslatef(0, 0, 1.0);
+		gluDisk(objDiskTop, 0, radius, 40, 50);
 
-        glEndList();
-    }
+		glEndList();
+	}
 
 	~cylinder() {
 		gluDeleteQuadric(objCylinder);
@@ -87,40 +95,29 @@ public:
 		gluDeleteQuadric(objDiskTop);
 	}
 
-    void render() override {
-        transform();
-        colorize();
+	void render() override {
+		transform();
+		colorize();
 
-        glPushMatrix();
-        glCallList(listId);
-        glPopMatrix();
-    }
+		glPushMatrix();
+		glCallList(listId);
+		glPopMatrix();
+	}
 };
 
 class cone : public node {
 private:
 	GLUquadricObj *objCylinder;
 	GLUquadricObj *objDiskTop;
+	GLint listId;
 
 public:
-    bool lid = true;
+	bool lid = true;
 
-    cone() {
-        center[2] = 0.5f;
+	cone() {
+		center[2] = 0.5f;
 		objCylinder = gluNewQuadric();
 		objDiskTop = gluNewQuadric();
-    }
-
-	~cone() {
-		gluDeleteQuadric(objCylinder);
-		gluDeleteQuadric(objDiskTop);
-	}
-
-    void render() override {
-        transform();
-        colorize();
-
-        const GLfloat radius = 0.5f;
 
 		gluQuadricDrawStyle(objCylinder, GLU_FILL);
 		gluQuadricTexture(objCylinder, GL_TRUE);
@@ -129,30 +126,49 @@ public:
 		gluQuadricTexture(objDiskTop, GL_TRUE);
 		gluQuadricNormals(objDiskTop, GLU_SMOOTH);
 
-        gluCylinder(objCylinder, 0, radius, 1.0, 40, 50);
-        if (lid) {
-            glPushMatrix();
-            glTranslatef(0, 0, 1.0);
-            gluDisk(objDiskTop, 0, radius, 40, 50);
-            glPopMatrix();
-        }
-    }
+		const GLfloat radius = 0.5f;
+
+		listId = glGenLists(1);
+		glNewList(listId, GL_COMPILE);
+
+		gluCylinder(objCylinder, 0, radius, 1.0, 40, 50);
+		glPushMatrix();
+		glTranslatef(0, 0, 1.0);
+		gluDisk(objDiskTop, 0, radius, 40, 50);
+		glPopMatrix();
+
+		glEndList();
+	}
+
+	~cone() {
+		gluDeleteQuadric(objCylinder);
+		gluDeleteQuadric(objDiskTop);
+	}
+
+	void render() override {
+		transform();
+		colorize();
+
+		glPushMatrix();
+		glCallList(listId);
+		glPopMatrix();
+	}
 };
 
 class prism : public node {
 private:
-    void drawPrism();
+	void drawPrism();
 
 public:
-    prism() {
-        center[0] = center[1] = center[2] = 0.5f;
-    }
+	prism() {
+		center[0] = center[1] = center[2] = 0.5f;
+	}
 
-    void render() override {
-        transform();
-        colorize();
-        drawPrism();
-    }
+	void render() override {
+		transform();
+		colorize();
+		drawPrism();
+	}
 };
 
 class frustum : public node {
